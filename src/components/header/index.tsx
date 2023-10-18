c9aa4541fa948fb76c7ffeae230015e6caa61f8b
Lashe <adeola5678@gmail.com>
2023-10-18 16:25:04 +0100

    fix: updates

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -35,25 +35,14 @@ const Header = ({
         </Link>
       </div>
       <>
-        {!auth.user ? (
-          <>
-            <label
-              className='btn bg-[#014421] h-[48px] px-5 lg:h-[50px] font-bold text-base lg:text-[20px] text-[#F5F5DC] rounded-xl'
-              htmlFor='modal-1'
-            >
-              {' '}
-              Register
-            </label>
-            <input className='modal-state' id='modal-1' type='checkbox' />
-          </>
-        ) : (
-          <button
-            type='submit'
-            className='btn w-full max-w-[200px] flex items-center justify-center bg-[#014421] h-[48px] px-5 lg:h-[50px] font-bold text-base lg:text-[20px] text-[#F5F5DC] rounded-xl'
-          >
-            Connect Wallet
-          </button>
-        )}
+        <label
+          className='btn bg-[#014421] h-[48px] px-5 lg:h-[50px] font-bold text-base lg:text-[20px] text-[#F5F5DC] rounded-xl'
+          htmlFor='modal-1'
+        >
+          {' '}
+          Register
+        </label>
+        <input className='modal-state' id='modal-1' type='checkbox' />
       </>
       <RegisterForm navigator={navigator} />
     </section>
